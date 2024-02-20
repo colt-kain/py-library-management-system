17b2909e30a77bc1c02980ae6daa9dc0193f9645
coltkain <kain.coltx@gmail.com>
2024-02-21 00:03:03 +0200

    feat: create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+from library import Library
+from book import Book
+from library_member import LibraryMember
+
+from utility_functions import *
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
