2e5a3a1e406df44700c7060ee2370496c339deac
coltkain <kain.coltx@gmail.com>
2024-02-21 00:01:37 +0200

    fix: format the code

--- book.py
+++ book.py
@@ -1,5 +1,5 @@
 class Book:
-    """
+    '''
     This class represents a book inside of a library.
 
     Attributes:
@@ -9,23 +9,15 @@ class Book:
         genre (str): The genre of the book.
         publication_year (int): The year when the book was published.
         pages_number (int): The total number of pages in the book.
-        is_available (bool): Indicates whether the book is available for checkout.
+        is_available (bool): Indicates whether the book is available for borrowing.
 
     Methods:
         display_information(): Displays detailed information about the book.
-    """
+    '''
 
-
-    def __init__(
-        self,
-        title: str,
-        author: str,
-        isbn: str,
-        genre: str,
-        publication_year: int,
-        pages_number: int,
-        is_available: bool
-    ) -> None:
+    def __init__(self, title: str, author: str,
+                 isbn: str, genre: str, publication_year: int,
+                 pages_number: int, is_available: bool) -> None:
         self.title = title
         self.author = author
         self.isbn = isbn
@@ -33,11 +25,10 @@ class Book:
         self.publication_year = publication_year
         self.pages_number = pages_number
         self.is_available = is_available
-
+        
 
     def display_information(self) -> None:
         print(f'[{self.isbn}] - "{self.title}" ({self.publication_year}):')
         print(f'* Author: {self.author}')
         print(f'* Genre: {self.genre}')
-        print(f'* Number of pages: {self.pages_number}')
-        print()
+        print(f'* Number of pages: {self.pages_number}\n')
